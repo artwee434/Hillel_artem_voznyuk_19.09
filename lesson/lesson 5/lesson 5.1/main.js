bd116edeb6e6d67284ad8df7574740075805ad90
Artem <artem.voznyuk333@gmail.com>
2025-10-07 16:58:20 +0300

    lesson 5

--- lesson/lesson 5/lesson 5.1/main.js
+++ lesson/lesson 5/lesson 5.1/main.js
@@ -1,6 +1,6 @@
 let result = "";
 
-for (let i = 10; i <= 20; i++) {
+for (let i = 10; i <= 20; i +=0.5) {
   result += i < 20 ? `${i}, ` : i;
 }
 
--- lesson/lesson 5/lesson 5.4/main.js
+++ lesson/lesson 5/lesson 5.4/main.js
@@ -1,6 +1,11 @@
-let number = prompt("...");
+let number = +prompt("Введіть число:"); 
 
 for (var i = 2; i < number; i++) {
   if (number % i === 0) break;
 }
-console.log(i === number ? number + " — просте" : number + " — не просте");
+
+if (number <= 1) {
+  console.log(number + " — не просте");
+} else {
+  console.log(i === number ? number + " — просте" : number + " — не просте");
+}
